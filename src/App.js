d22ecda9e642d2b74a9ce71b5dcbaab4f149e301
sigier <sergiigolinka@gmail.com>
2021-05-23 11:59:29 +0300

    add shop page,  collection-preview, collection-item components

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import React from 'react';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter , Switch, Route } from 'react-router-dom';
+import ShopPage from './pages/shop/shop.component';
 
 function App() {
   return (
     <BrowserRouter>
     <div>
-     <HomePage/>
+     <Switch>
+       <Route exact path="/" component={HomePage}></Route>
+       <Route path="/shop" component={ShopPage}></Route>
+     </Switch>
     </div>
     </BrowserRouter>
-    
   );
 }
 
