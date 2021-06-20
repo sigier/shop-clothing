41b1041eaa86374b1f3ec11d4a15f5c6fafb116f
sigier <sergiigolinka@gmail.com>
2021-06-20 09:56:34 +0300

    Add redirect to main page after sign-in completion

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component';
 import SignInSignUpPage from './pages/signin-and-signup-page/signin-and-signup-page.component';
 import { auth, createUserProfileDocument } from './firebase/firebase.utils';
-import { setCurrentUser } from './redux/user/user-actions';
+import { setCurrentUser } from './redux/user/user.actions';
 
 
 class App extends React.Component {
@@ -49,7 +49,12 @@ class App extends React.Component {
           <Switch>
             <Route exact path="/" component={HomePage}></Route>
             <Route path="/shop" component={ShopPage}></Route>
-            <Route path="/signin" component={SignInSignUpPage}></Route>
+            <Route exact path='/signin'
+              render={() => this.props.currentUser
+                ? (<Redirect to='/' />)
+                : (<SignInSignUpPage />)
+              }
+            />
           </Switch>
         </div>
       </BrowserRouter>
@@ -58,8 +63,12 @@ class App extends React.Component {
 
 }
 
+const mapStateToProps = ({ user }) => ({
+  currentUser: user.currentUser
+});
+
 const mapDispatchToProps = dispatch => ({
   setCurrentUser: user => dispatch(setCurrentUser(user))
 });
 
-export default connect(null, mapDispatchToProps)(App);
+export default connect(mapStateToProps, mapDispatchToProps)(App);
