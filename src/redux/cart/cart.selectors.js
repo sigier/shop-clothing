18700d14e63611dc2837a8b148bfbb816d06ab80
sigier <sergiigolinka@gmail.com>
2021-06-21 17:58:04 +0300

    add TOTAL calc into checkout page

--- src/redux/cart/cart.selectors.js
+++ src/redux/cart/cart.selectors.js
@@ -17,3 +17,10 @@ export const selectCartItemsCount = createSelector(
     cartItems => cartItems.reduce((accumulated, cartItem) =>
         accumulated + cartItem.quantity, 0)
 );
+
+export const selectCartTotal = createSelector(
+    [selectCartItems],
+    cartItems => cartItems.reduce((accumulated, cartItem) =>
+        accumulated + cartItem.quantity * cartItem.price, 0)
+
+);
