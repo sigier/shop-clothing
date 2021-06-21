a7d57cb9930f9748a889a3f37bd58f66fa8eacb0
sigier <sergiigolinka@gmail.com>
2021-06-21 15:53:13 +0300

    implement createStructuredSelector

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
 
 import { toggleCartHidden } from '../../redux/cart/cart.actions';
 import { selectCartItemsCount } from '../../redux/cart/cart.selectors';
@@ -19,9 +20,9 @@ const mapDispatchToProps = dispatch => ({
   toggleCartHidden: () => dispatch(toggleCartHidden())
 });
 
-const mapStateToProps = (state) => (
+const mapStateToProps = createStructuredSelector(
   {
-    itemCount: selectCartItemsCount(state)
+    itemCount: selectCartItemsCount 
   }
 );
 
--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -7,8 +7,12 @@ import CartIcon from '../cart-icon/cart-icon.component';
 import CartDropdown from '../cart-dropdown/cart-dropdown.component';
 
 import { ReactComponent as Logo } from '../../assets/crown.svg';
+import { createStructuredSelector } from 'reselect';
 
 import './header.styles.scss';
+import { selectCartHidden } from '../../redux/cart/cart.selectors';
+import { selectCurrentUser } from '../../redux/user/user.selectors';
+
 
 const Header = ({ currentUser, hidden }) => (
   <div className='header'>
@@ -37,9 +41,9 @@ const Header = ({ currentUser, hidden }) => (
   </div>
 );
 
-const mapStateToProps = ({ user: { currentUser }, cart: { hidden } }) => ({
-  currentUser,
-  hidden
+const mapStateToProps =  createStructuredSelector({
+  currentUser: selectCurrentUser,
+  hidden: selectCartHidden 
 });
 
 export default connect(mapStateToProps)(Header);
